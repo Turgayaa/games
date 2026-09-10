dc123f7bdf3f9914d413cf3508abfb4e370485c1
Turgay <2245727@talnet.com>
2026-09-10 13:33:06 +0200

    Create: formulier en store

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -5,29 +5,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
     <title>Game Collection</title>
-    <th>Genre</th>
 </head>
 <body>
-    <div class="container" style="margin:40px;">
-        <h1 class="display-4">🎮 Game Collection</h1>
-        <table class="table">
+    <div class="container mt-5">
+        <h1 class="display-4 mb-4">🎮 Game Collection</h1>
+
+        <table class="table table-striped">
             <thead class="thead-dark">
                 <tr>
                     <th>ID</th>
                     <th>Game</th>
                     <th>Platform</th>
+                    <th>Genre</th>
                     <th>Rating</th>
                 </tr>
-                <td>{{ $game->genre }}</td>
             </thead>
+
             <tbody>
-                @foreach($games as $game) {{-- in deze loop worden alle rijen (records) gemaakt die in de database zijn gevonden. --}}
-                    <tr>
-                        <td>{{ $game->id }}</td>
-                        <td>{{ $game->game_name }}</td>
-                        <td>{{ $game->platform }}</td>
-                        <td>{{ $game->rating }}/10</td> 
-                    </tr>
+                @foreach($games as $game)
+                <tr>
+                    <td>{{ $game->id }}</td>
+                    <td>{{ $game->game_name }}</td>
+                    <td>{{ $game->platform }}</td>
+                    <td>{{ $game->genre }}</td>
+                    <td>{{ $game->rating }}/10</td>
+                </tr>
                 @endforeach
             </tbody>
         </table>
