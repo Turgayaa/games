4c75e6ddfeb211efd05169cbdd557301e787d103
Turgay <2245727@talnet.com>
2026-09-22 11:41:29 +0200

    Games CRUD beveiligd met auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\GameController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,14 +12,38 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+/*
+|--------------------------------------------------------------------------
+| Iedereen mag deze zien
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/games', [GameController::class, 'index']);
+Route::get('/games/show/{id}', [GameController::class, 'show']);
+
+/*
+|--------------------------------------------------------------------------
+| Alleen ingelogde gebruikers
+|--------------------------------------------------------------------------
+*/
+
 Route::middleware('auth')->group(function () {
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
-require __DIR__.'/auth.php';
+    Route::get('/games/create', [GameController::class, 'create']);
+    Route::post('/games/store', [GameController::class, 'store']);
+
+    Route::get('/games/edit/{id}', [GameController::class, 'edit']);
+    Route::post('/games/update/{id}', [GameController::class, 'update']);
+
+    Route::post('/games/destroy/{id}', [GameController::class, 'destroy']);
+});
 
 Route::get('/geheim', function () {
     return view('geheim');
 })->middleware('auth');
+
+require __DIR__.'/auth.php';
